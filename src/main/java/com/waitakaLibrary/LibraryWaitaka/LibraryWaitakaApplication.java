15522be7b03085b32e0fb1e9511520eff0d140f6
Iago Duarte de Aguiar <iagoaguiar202@gmail.com>
2021-09-06 17:16:19 -0300

    Iniciado o projeto
    Projeto foi iniciado com configuracoes basicas no Spring Initializr

---
+++ src/main/java/com/waitakaLibrary/LibraryWaitaka/LibraryWaitakaApplication.java
@@ -0,0 +1,13 @@
+package com.waitakaLibrary.LibraryWaitaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWaitakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWaitakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waitakaLibrary/LibraryWaitaka/LibraryWaitakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waitakaLibrary.LibraryWaitaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWaitakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
